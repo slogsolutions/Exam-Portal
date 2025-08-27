ec2d6049ece881404ea430b24f445082edfc8e3a
Cocogoat28 <gvansh282@gmail.com>
2025-08-27 21:37:59 +0530

    changes

--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from registration import views
+from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
+    path("register/", views.register_candidate, name="register_candidate"),
      path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("dashboard/", views.candidate_dashboard, name="candidate_dashboard"),
--- registration/views.py
+++ registration/views.py
@@ -22,3 +22,21 @@ def candidate_dashboard(request):
         "completed_exams": completed_exams,
         "results": results,
     })
+from django.shortcuts import render, redirect
+from .forms import CandidateRegistrationForm
+
+def register_candidate(request):
+    if request.method == "POST":
+        
+        print("Form is valid")
+        form = CandidateRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            print("Form is valid and saved")
+            return redirect("login")  # Define this URL/view
+        else:
+            print("error")
+    else:
+        print("invalid")
+        form = CandidateRegistrationForm()
+    return render(request, "registration/register_candidate.html", {"form": form})
