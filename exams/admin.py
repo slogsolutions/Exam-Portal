273ceb83b49cbd9814f26876266f61e97ff6796f
Abhishek Rawat <rawatabhishek385@gmail.com>
2025-08-28 01:29:30 +0530

    okkk

--- exams/admin.py
+++ exams/admin.py
@@ -3,10 +3,9 @@ from .models import ExamDayAvailability, Shift
 
 @admin.register(ExamDayAvailability)
 class ExamDayAvailabilityAdmin(admin.ModelAdmin):
-    list_display = ['date']  # show date column
-    filter_horizontal = ['categories']  # if you have ManyToMany categories
+    list_display = ['date']
+    filter_horizontal = ['categories']  # if categories is ManyToMany
 
 @admin.register(Shift)
 class ShiftAdmin(admin.ModelAdmin):
     list_display = ['center', 'date', 'start_time', 'capacity']
-    list_filter = ['center','date']
